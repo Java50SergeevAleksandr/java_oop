6045a4cfeb3cbc9ba250f0c1eb94572cec2f1edb
AlGuron <blizgur8@gmail.com>
2023-06-12 18:28:53 +0300

    add HW#05

---
+++ src/telran/util/tests/EvenOddComparator.java
@@ -0,0 +1,26 @@
+package telran.util.tests;
+
+import java.util.Comparator;
+
+public class EvenOddComparator implements Comparator<Integer> {
+
+	@Override
+	public int compare(Integer o1, Integer o2) {
+		int res = -1;
+		if (isEven(o1)) {
+			if (isEven(o2)) {
+				res = o1 - o2;
+			}
+		} else {
+			res = 1;
+			if (!isEven(o2)) {
+				res = o2 - o1;
+			}
+		}
+		return res;
+	}
+
+	private boolean isEven(Integer o1) {
+		return Math.abs(o1) % 2 > 0 ? false : true;
+	}
+}
